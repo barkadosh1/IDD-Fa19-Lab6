cf0ffa0247131e4df24ca43bd88e5c7868470585
Bar Kadosh <kadoshraspberry@gmail.com>
2019-10-21 18:33:59 -0700

    Added new intro message.

--- chatServer.js
+++ chatServer.js
@@ -10,7 +10,6 @@ var http = require('http').Server(app); // connects http library to server
 var io = require('socket.io')(http); // connect websocket library to server
 var serverPort = 8000;
 
-
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
 app.use(express.static('public')); // find pages in public directory
@@ -30,8 +29,11 @@ io.on('connect', function(socket) {
   var questionNum = 0; // keep count of question, used for IF condition.
   socket.on('loaded', function() { // we wait until the client has loaded and contacted us that it is ready to go.
 
-    socket.emit('answer', "Hey, hello I am \"___*-\" a simple chat bot example."); //We start with the introduction;
-    setTimeout(timedQuestion, 5000, socket, "What is your name?"); // Wait a moment and respond with a question.
+
+    socket.emit('changeBG', 'green');
+    socket.emit('changeFont', 'white')
+    socket.emit('answer', "Hi! I am TunesBot, a simple chat bot that recommends music catered to your situation."); //We start with the introduction;
+    setTimeout(timedQuestion, 6500, socket, "What is your name?"); // Wait a moment and respond with a question.
 
   });
   socket.on('message', (data) => { // If we get a new message from the client we process it;
@@ -43,51 +45,211 @@ io.on('connect', function(socket) {
   });
 });
 //--------------------------CHAT BOT FUNCTION-------------------------------//
+var moving_val = 0;
+
 function bot(data, socket, questionNum) {
   var input = data; // This is generally really terrible from a security point of view ToDo avoid code injection
   var answer;
   var question;
   var waitTime;
+  var song1 = "Happy by Pharrell Williams";
+  var song2 = "Gravity By Sara Bareilles";
+  var song3 = "Let It Go From Disney’s Frozen";
+  var song4 = "Weather by Novo Amor";
+  var song5 = "No Brainer by Justin Bieber";
+  var song6 = "Levels by Avicii";
+  var song7 = "Family Business by Kanye West";
+  var song8 = "Day Glo by Brazos";
+  var song9 = "Brokedown by The California Honeydrops";
+  var song10 = "Such Great Heights by Iron and Wine";
+  var song11 = "Colorado by Lotus";
 
   /// These are the main statments that make up the conversation.
   if (questionNum == 0) {
-    answer = 'Hello ' + input + ' :-)'; // output response
-    waitTime = 5000;
-    question = 'How old are you?'; // load next question
-  } else if (questionNum == 1) {
-    answer = 'Really, ' + input + ' years old? So that means you were born in: ' + (2018 - parseInt(input)); // output response
-    waitTime = 5000;
-    question = 'Where do you live?'; // load next question
-  } else if (questionNum == 2) {
-    answer = 'Cool! I have never been to ' + input + '.';
-    waitTime = 5000;
-    question = 'Whats your favorite color?'; // load next question
-  } else if (questionNum == 3) {
-    answer = 'Ok, ' + input + ' it is.';
-    socket.emit('changeBG', input.toLowerCase());
-    waitTime = 5000;
-    question = 'Can you still read the font?'; // load next question
-  } else if (questionNum == 4) {
-    if (input.toLowerCase() === 'yes' || input === 1) {
-      answer = 'Perfect!';
+    answer = 'Hello ' + input + ' :-). By answering the following questions for me, I will best be able to recommend music to you!'; // output response
+    waitTime = 6500;
+    question = 'What activity are you trying to do: relax, workout, or study?'; // load next question
+    questionNum = 1;
+  }
+
+  else if (moving_val == 0) {
+    if (input.toLowerCase()  == "relax") {
+      answer = "Awesome, let's find you a song that will help you relax!"; // output response
       waitTime = 5000;
-      question = 'Whats your favorite place?';
-    } else if (input.toLowerCase() === 'no' || input === 0) {
-      socket.emit('changeFont', 'white'); /// we really should look up the inverse of what we said befor.
-      answer = ''
-      question = 'How about now?';
-      waitTime = 0;
-      questionNum--; // Here we go back in the question number this can end up in a loop
-    } else {
-      question = 'Can you still read the font?'; // load next question
-      answer = 'I did not understand you. Could you please answer "yes" or "no"?'
-      questionNum--;
-      waitTime = 5000;
+      question = 'How are you feeling: happy, sad, or angry?'; // load next question
+      moving_val = 1;
     }
-    // load next question
-  } else {
-    answer = 'I have nothing more to say!'; // output response
-    waitTime = 0;
+    else if (input.toLowerCase()  == "workout") {
+      answer = "Awesome, let's find you a song that will help you workout!"; // output response
+      waitTime = 5000;
+      question = 'Are you: lifting or jogging?'; // load next question
+      moving_val = 2;
+    }
+    else if (input.toLowerCase()  == "study") {
+      answer = "Awesome, let's find you a song that will help you study!"; // output response
+      waitTime = 5000;
+      question = 'Are you studying: alone or with friends?'; // load next question
+      moving_val = 3;
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you want to relax, workout, or study!"; // output response
+      waitTime = 5000;
+      question = 'What activity are you trying to do: relax, workout, or study?'; // load next question
+    }
+  }
+
+  else if (moving_val == 1) {
+    if (input.toLowerCase()  == "happy") {
+      answer = "I'm HAPPY to hear that you are HAPPY! I would recommend the song " + song1 + "  to you ;)"; // output response
+      waitTime = 7000;
+      moving_val = -1;
+    }
+    else if (input.toLowerCase()  == "sad") {
+      answer = "I'm sorry to hear you are feeling sad :( I think the song " + song2 + "  might help calm you down and feel better :)"; // output response
+      waitTime = 7000;
+       moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else if (input.toLowerCase()  == "angry") {
+      answer = "Anger is a terrible feeling to have. I think the song " + song3 + "  might help you throw away your anger and adopt a new perspective!"; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you are happy, sad, or angry!"; // output response
+      waitTime = 5000;
+      question = 'How are you feeling: happy, sad, or angry?'; // load next question
+    }
+  }
+
+  else if (moving_val == 2) {
+    if (input.toLowerCase()  == "lifting") {
+      answer = "Awesome, let's find you a song that will help you lift!"; // output response
+      waitTime = 5000;
+      question = 'When you lift, do you enjoy music that is: intense or mellow?'; // load next question
+      moving_val = 4;
+    }
+    else if (input.toLowerCase()  == "jogging") {
+      answer = "Awesome, let's find you a song that will help you jog!"; // output response
+      waitTime = 5000;
+      question = 'Are you jogging: outside or inside?'; // load next question
+      moving_val = 5;
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you want to go jogging or lifting!"; // output response
+      waitTime = 5000;
+      question = 'Are you: lifting or jogging?'; // load next question
+    }
+  }
+
+
+  else if (moving_val == 3) {
+    if (input.toLowerCase()  == "alone") {
+      answer = "Sounds lonely :( Let's find you a song to make studying more enjoyable :)"; // output response
+      waitTime = 5000;
+      question = 'When you study, do you like music: with words or without words?'; // load next question
+      moving_val = 6;
+    }
+    else if (input.toLowerCase()  == "with friends") {
+      answer = "Friends + Good Tunes = GREAT Times!"; // output response
+      waitTime = 5000;
+      question = 'Do you and your friends want background music that is: mellow or upbeat?'; // load next question
+      moving_val = 7;
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you are studying alone or with friends!"; // output response
+      waitTime = 5000;
+      question = 'Are you studying: alone or with friends?'; // load next question
+    }
+  }
+
+
+  else if (moving_val == 4) {
+    if (input.toLowerCase()  == "intense") {
+      answer = "GO GO GO! A good song for you would be " + song6 + "."; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else if (input.toLowerCase()  == "mellow") {
+      answer =  song7 + " should be a good option for a more mellow lifting session. But that doesn't mean you can skip legs!"; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you want your workout music to be mellow or intense!"; // output response
+      waitTime = 5000;
+      question = 'When you lift, do you enjoy music that is: intense or mellow?'; // load next question
+    }
+  }
+
+  else if (moving_val == 5) {
+    if (input.toLowerCase()  == "outside") {
+      answer = "That sounds lovely :) Let's give you a more soothing song that will allow you to enjoy the beautiful scenery of the outdoors: " + song4 + "."; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else if (input.toLowerCase()  == "inside") {
+      answer = "Bad weather ruins everything :/ A good song to combat the gloomy weather is " + song5 + "." ; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you are jogging inside or outside!"; // output response
+      waitTime = 5000;
+      question = 'Are you jogging: outside or inside?'; // load next question
+    }
+  }
+
+  else if (moving_val == 6) {
+    if (input.toLowerCase()  == "with words") {
+      answer = "Is a song really a song without words?! Here is a mellow song that will calm you while you study: " + song10 + "."; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else if (input.toLowerCase()  == "without words") {
+      answer = "Words distract me too! Here is a calming song with a smooth guitar melody: " + song11 + "."; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you want music with or without words!"; // output response
+      waitTime = 5000;
+      question = 'When you study, do you like music: with words or without words?'; // load next question   
+    }
+  }
+
+  else if (moving_val == 7) {
+    if (input.toLowerCase()  == "mellow") {
+      answer = "Don't want music that will distract you too much! " + song8 + " is a good one -- check it out."; // output response
+      waitTime = 7000;
+      moving_val = -1;
+      question = "Which was your favorite song?";
+    }
+    else if (input.toLowerCase()  == "upbeat") {
+      answer = "Get pumped! Here is a fun one that I love: " + song9 + "."; // output response
+      waitTime = 7000;
+      moving_val = -1; 
+      question = "Which was your favorite song?";
+    }
+    else {
+      answer = "I don't recognize this activity. Please tell me if you want upbeat or mellow music!"; // output response
+      waitTime = 5000;
+      question = 'Do you and your friends want background music that is: mellow or upbeat?'; // load next question
+    }
+  }
+
+
+
+  else {
+    answer = 'Hope you are enjoying those tunes!'; // output response
+    waitTime = 5000;
     question = '';
   }
 
